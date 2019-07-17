183b17541cbd28ae8fcb136029d65b244f4f2ea1
yarsoniy <yarsoniy@gmail.com>
2019-07-17 23:24:23 +0300

    Param convert to DTO

--- src/Controller/Rest/TestController.php
+++ src/Controller/Rest/TestController.php
@@ -2,7 +2,9 @@
 
 namespace Company\Split\Controller\Rest;
 
+use Company\Split\Controller\Rest\DTO\TestDTO;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations as Rest;
 
@@ -21,6 +23,20 @@ class TestController extends AbstractFOSRestController
         ];
 
         $view = $this->view($data, Response::HTTP_OK);
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Rest\Post("/test")
+     * @ParamConverter("testDTO", converter="fos_rest.request_body")
+     * @param TestDTO $testDTO
+     * @return Response
+     */
+    public function postTestAction(TestDTO $testDTO)
+    {
+        $debug = true;
+
+        $view = $this->view($testDTO, Response::HTTP_OK);
         return $this->handleView($view);
     }
 }
